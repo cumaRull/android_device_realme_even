dcd952d43e0dac1be1e476c6c30d886cb1d6007d
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-27 14:32:50 +0530

    even: Import framework compatibility matrix
    * sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 RMX2020_11_C.06 release-keys
    
    Change-Id: I3960902ac07c468e6db0d386179d707ee565e116
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ BOARD_HAS_MTK_HARDWARE := true
 BUILD_BROKEN_DUP_RULES := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
