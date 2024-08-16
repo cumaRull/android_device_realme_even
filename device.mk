7718726fc431fe7e54d7f51857b00aeca28c7368
From: TheMalachite <eliasgheeraert@gmail.com>
2024-08-16 15:37:28 +0000

    even: Import Wi-fi configs
    Change-Id: I0f155e5111a8c3fa1988bc1768f2dc38de84c1d1
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- device.mk
+++ device.mk
@@ -255,5 +255,9 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
-    wpa_supplicant.conf \
     WifiOverlay
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
